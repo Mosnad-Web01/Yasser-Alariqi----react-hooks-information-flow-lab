5fae4e82cd937688318096c9599f349fd4adba17
YasserEsam <yaaser10esam1010@gmail.com>
2024-09-04 23:50:05 +0300

    pass the test and refactor the code to seperate components from parent component

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,16 @@
 import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
+import Header from "./Header";
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
 
-  function onDarkModeClick() {
-    setIsDarkMode((isDarkMode) => !isDarkMode);
-  }
+  const appClass = isDarkMode ? "App dark" : "App light";
 
   return (
-    <div className={"App " + (isDarkMode ? "dark" : "light")}>
-      <header>
-        <h2>Shopster</h2>
-        <button onClick={onDarkModeClick}>
-          {isDarkMode ? "Dark" : "Light"} Mode
-        </button>
-      </header>
+    <div className={appClass}>
+      <Header isDarkMode={isDarkMode} onDarkModeClick={setIsDarkMode} />
       <ShoppingList items={itemData} />
     </div>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Header({ onDarkModeClick, isDarkMode }) {
+
+    function handleDarkModeClick() {
+        onDarkModeClick((prevMode) => !prevMode);
+    }
+
+  return (
+    <header>
+        <h2>Shopster</h2>
+        <button onClick={handleDarkModeClick}>
+        {isDarkMode ? "Dark" : "Light"} Mode
+        </button>
+    </header>
+  )
+}
+
+export default Header;
